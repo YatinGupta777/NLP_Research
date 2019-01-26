016ad8d9ef1f9ca4eed6431df24dbe9cd6658a78
Yat-Mac <yatingupta7777@gmail.com>
2019-01-26 15:20:23 +0530

    POS tagged and stop words removed
    Imported dataset from NLTK. (Uploaded one is for reference)
    IT right now prints the entire dataset POS Tagged

--- MAIN_program.py
+++ MAIN_program.py
@@ -1,24 +1,12 @@
-
-#POS TAGGING AND STOP WORDS REMOVAL"
+from nltk import Text
+from nltk.corpus import reuters
 import nltk 
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize, sent_tokenize 
 stop_words = set(stopwords.words('english')) 
-  
-# Dummy text 
-txt = "Sukanya, Rajib and Naba are my good friends. " \ 
-    "Sukanya is getting married next year. " \ 
-    "Marriage is a big step in one’s life." \ 
-    "It is both exciting and frightening. " \ 
-    "But friendship is a sacred bond between people." \ 
-    "It is a special kind of love between us. " \ 
-    "Many of you must have tried searching for a friend "\ 
-    "but never found the right one."
-  
-# sent_tokenize is one of instances of  
-# PunktSentenceTokenizer from the nltk.tokenize.punkt module 
-  
-tokenized = sent_tokenize(txt) 
+ 
+tokenized = reuters.words()
+ 
 for i in tokenized: 
       
     # Word tokenizers is used to find the words  
@@ -33,3 +21,25 @@ for i in tokenized:
     tagged = nltk.pos_tag(wordsList) 
   
     print(tagged) 
+
+# Get the collocations that don't contain stop-words
+#text.collocations() 
+# United States; New York; per cent; Rhode Island; years ago; Los Angeles; White House; ...
+ 
+# Get words that appear in similar contexts
+#text.similar('Monday', 5) 
+# april march friday february january
+ 
+# Get common contexts for a list of words
+#text.common_contexts(['August', 'June']) 
+# since_a in_because last_when between_and last_that and_at ...
+ 
+# Get contexts for a word
+#text.concordance('Monday')
+# said . Trade Minister Saleh said on Monday that Indonesia , as the world ' s s
+# Reuters to clarify his statement on Monday in which he said the pact should be
+#  the 11 - member CPA which began on Monday . They said producers agreed that c
+# ief Burkhard Junger was arrested on Monday on suspicion of embezzlement and of
+# ween one and 1 . 25 billion dlrs on Monday and Tuesday . The spokesman said Mo
+# ay and Tuesday . The spokesman said Monday ' s float included 500 mln dlrs in 
+ 
