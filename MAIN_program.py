91b5abbcab175377d5d4da9ddf0acd0b5f416ba7
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-01-28 14:20:43 +0530

    FCM data set generated, applied fcm & applied fpcs

--- MAIN_program.py
+++ MAIN_program.py
@@ -1,3 +1,6 @@
+
+    
+from __future__ import division, print_function
 from nltk import Text
 from nltk.corpus import reuters
 import nltk 
@@ -6,6 +9,10 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import wordnet
 import collections, re
+
+import numpy as np
+import matplotlib.pyplot as plt
+import skfuzzy as fuzz
 stop_words = set(stopwords.words('english')) 
  
 #tokenized = reuters.words()
@@ -69,6 +76,64 @@ for i in bagsofwords:
         
 for i in bagsofwords:
     print (i) 
+    
+
+colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
+
+# Define three cluster centers
+centers = [[4, 2],
+           [1, 7],
+           [5, 6]]
+
+# Define three cluster sigmas in x and y, respectively
+sigmas = [[0.8, 0.3],
+          [0.3, 0.5],
+          [1.1, 0.7]]
+
+# Generate test data
+np.random.seed(42)  # Set seed for reproducibility
+xpts = np.zeros(1)
+ypts = np.zeros(1)
+labels = np.zeros(1)
+for j in bagofwords:
+    for i, ((xmu, ymu), (xsigma, ysigma)) in enumerate(zip(centers, sigmas)):
+        xpts = np.hstack((xpts, bagofwords[j]* xsigma + xmu))
+        ypts = np.hstack((ypts, bagofwords[j] * ysigma + ymu))
+        labels = np.hstack((labels, np.ones(200) * i))
+
+# Visualize the test data
+fig0, ax0 = plt.subplots()
+for label in range(3):
+    ax0.plot(xpts[labels == label], ypts[labels == label], '.',
+             color=colors[label])
+ax0.set_title('Test data: 200 points x3 clusters.')
+
+# Set up the loop and plot
+fig1, axes1 = plt.subplots(3, 3, figsize=(8, 8))
+alldata = np.vstack((xpts, ypts))
+fpcs = []
+
+for ncenters, ax in enumerate(axes1.reshape(-1), 2):
+    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+        alldata, ncenters, 2, error=0.005, maxiter=100, init=None)
+
+    # Store fpc values for later
+    fpcs.append(fpc)
+
+    # Plot assigned clusters, for each data point in training set
+    cluster_membership = np.argmax(u, axis=0)
+    for j in range(ncenters):
+        ax.plot(xpts[cluster_membership == j],
+                ypts[cluster_membership == j], '.', color=colors[j])
+
+    # Mark the center of each fuzzy cluster
+    for pt in cntr:
+        ax.plot(pt[0], pt[1], 'rs')
+
+    ax.set_title('Centers = {0}; FPC = {1:.2f}'.format(ncenters, fpc))
+    ax.axis('off')
+
+fig1.tight_layout()
 #print(document_vector)
 
 # Get the collocations that don't contain stop-words
