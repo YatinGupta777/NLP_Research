99dc0b43c21c9508e53b938f90665972935f00b5
Yat-Mac <yatingupta7777@gmail.com>
2019-03-22 10:52:03 +0530

    Made Wordnet graph
    using hypernymy and hyponym of words

--- query_expansion_fuzzy.py
+++ query_expansion_fuzzy.py
@@ -15,10 +15,16 @@ import numpy, scipy.io
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import networkx as nx
+from textblob import Word
+import re
+str = "123456790abcdefABCDEF!@#$%^&*()_+<>?,./"
+G=nx.Graph()
 f = open("questions.txt","r")
 fout = open("QUERIES_for_training_Expanded","w", encoding="utf-8")
 stop_words=set(stopwords.words("english"))
 
+test_string = "Homemade glass cleaner?"
 
 def stemming(word):
     
@@ -27,40 +33,79 @@ def stemming(word):
     stemmed = porter.stem(word)
          
     return stemmed   
+'''For Test String'''
 
-with open('questions.txt',encoding="ISO-8859-1",newline='') as f:
-   
-    for line in f:
+test_string = test_string.replace('\n','')
+''.join([i for i in test_string if i.isalpha()])
+wordsList = nltk.word_tokenize(test_string) 
+filtered_sentence = [w for w in wordsList if not w in stop_words]
+for i in filtered_sentence:
+    i = stemming(i)
+
+for x in filtered_sentence:
+    word = Word(x)
+    if(len(word.synsets) > 1):
+        w = word.synsets[1]
     
-        if line and not line.startswith("<"):
-            #print(line)
-            line=line.replace('\n','')
-            wordsList = nltk.word_tokenize(line) 
-            filtered_sentence = [w for w in wordsList if not w in stop_words]
-            for i in filtered_sentence:
-                stemming(i)
-            synonyms=[]
-            count=0
-            for x in filtered_sentence:
-                
-                for syn in wordnet.synsets(x):
-                    for l in syn.lemmas() :
-                        if(count<3):
-                            if l.name() not in synonyms:
-                                synonyms.append(l.name())
-                                count+=1
-                                
-                count=0
+        G.add_node(w.name())
+        for h in w.hypernyms():
+            print (h)
+            G.add_node(h.name())
+            G.add_edge(w.name(),h.name())
             
-        synonyms_string=' '.join(synonyms)
-        synonyms=[]
-        fout.write(synonyms_string)
-        fout.write('\n')
+        for h in w.hyponyms():
+            print (h)
+            G.add_node(h.name())
+            G.add_edge(w.name(),h.name())
 
-		
+'''For Whole file'''
+# =============================================================================
+# with open('questions.txt',encoding="ISO-8859-1",newline='') as f:
+#    
+#     for line in f:
+#     
+#         if line and not line.startswith("<"):
+#             #print(line)
+#             line=line.replace('\n','')
+#             wordsList = nltk.word_tokenize(line) 
+#             filtered_sentence = [w for w in wordsList if not w in stop_words]
+#             for i in filtered_sentence:
+#                 stemming(i)
+# 
+#             for x in filtered_sentence:
+#                 word = Word(x)
+#                 if(len(word.synsets) > 1):
+#                     w = word.synsets[1]
+#                 
+#                 G.add_node(w.name())
+#                 for h in w.hypernyms():
+#                     #print (h)
+#                     G.add_node(h.name())
+#                     G.add_edge(w.name(),h.name())
+#                     
+#                 for h in w.hyponyms():
+#                     #print (h)
+#                     G.add_node(h.name())
+#                     G.add_edge(w.name(),h.name())
+# =============================================================================
+            
+# =============================================================================
+#         synonyms_string=' '.join(synonyms)
+#         synonyms=[]
+#         fout.write(synonyms_string)
+#         fout.write('\n')
+# =============================================================================
+
+print (G.nodes(data=True))
+plt.show()
+nx.draw(G, width=2, with_labels=True)
+#plt.savefig("path.png")
+
+
 f.close()
 fout.close()
 
 
 #Source
+#http://intelligentonlinetools.com/blog/2016/09/05/getting-wordnet-information-and-building-and-building-graph-with-python-and-networkx/
 #https://github.com/ellisa1419/Wordnet-Query-Expansion
